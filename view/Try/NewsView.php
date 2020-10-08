314ad21a2d30f0dcaef4228c378f2e2216913e22
NonozgYtb <nonozg.ytb@gmail.com>
2020-10-08 13:44:43 +0200

    2020.10.08b

--- view/Try/NewsView.php
+++ view/Try/NewsView.php
@@ -1,7 +1,7 @@
 <?= \App\Render::getInstance()->setTemplate("test") ?>
 
 <!-- Header -->
-<section class="bg-sizer new-header" data-bg-url="/img/bg.jpg">
+<section class="bg-sizer new-header bg-secondary" data-bg-url="/img/bg.jpg">
     <div class="row mx-0">
         <h2 class="color-white col-12 col-md-6 col-lg-5 col-xl-4 text-truncate">kytuytjerreutriètydsjrersu-e(ièyjsrtu(tjsrhey(usrkyrjtyè(ksyrjtèktsuh,-</h2>
         <div class="col-12"></div>
@@ -13,34 +13,17 @@
 <section class="bg-secondary color-white page-section py-5" id="services">
     <div class="container">
         <div class="row m-0">
-            <div class="col-12 col-md-8 col-xl-9 bg-success" style="height: 300px"></div>
-            <div class="col-12 m-0 pt-3 pt-md-0 col-md-4 col-xl-3 bg-danger" style="">
-                <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">List group item heading</h5>
-                            <small>3 days ago</small>
-                        </div>
-                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                        <small>Donec id elit non mi porta.</small>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">List group item heading</h5>
-                            <small class="text-muted">3 days ago</small>
-                        </div>
-                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                        <small class="text-muted">Donec id elit non mi porta.</small>
-                    </a>
-                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">List group item heading</h5>
-                            <small class="text-muted">3 days ago</small>
-                        </div>
-                        <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                        <small class="text-muted">Donec id elit non mi porta.</small>
-                    </a>
-                </div>
+            <div class="col-12 col-md-8 col-xl-9" style="height: 300px"></div>
+            <div class="col-12 m-0 pt-3 pt-md-0 col-md-4 col-xl-3">
+                <ul class="list-group list-group-flush spearater-white ul-transparent">
+                    <li class="list-group-item"><a href="" class="color-white">Cras justo odio</a></li>
+                    <li class="list-group-item li-header not-flush">Dapibus ac facilisis in</li>
+                    <li class="list-group-item not-flush"><a href="" class="color-white">Morbi leo risus</a></li>
+                    <li class="list-group-item not-flush"><a href="" class="color-white">Lorem ipsum dolor, sit amet.</a></li>
+                    <li class="list-group-item"><a href="" class="color-white">Porta ac consectetur ac</a></li>
+                    <li class="list-group-item not-flush"><a href="" class="color-white">Vestibulum at eros</a></li>
+                    <li class="list-group-item"><a href="" class="color-white">Cras justo odio</a></li>
+                </ul>
             </div>
         </div>
     </div>
